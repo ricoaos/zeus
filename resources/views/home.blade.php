4efcdc4e05fa421be1110549dab3f48364fb3bac
Ricardo Alexandre <ricardo.alexandre@basis.com.br>
2018-11-28 09:44:38 -0200

    'Adicionando'

---
+++ resources/views/home.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+            <div class="hidden-top">
+      <div class="hidden-top-inner container">
+        <div class="row">
+          <div class="span12">
+            <ul>
+              <li><strong>We are available for any custom works this month</strong></li>
+              <li>Main office: Springville center X264, Park Ave S.01</li>
+              <li>Call us <i class="icon-phone"></i> (123) 456-7890 - (123) 555-7891</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+                <div class="panel-heading">Dashboard</div>
+
+                <div class="panel-body">
+                    @if (session('status'))
+                        <div class="alert alert-success">
+                        
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    You are logged in!
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
